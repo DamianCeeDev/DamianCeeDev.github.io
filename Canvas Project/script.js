e6e5cc51db4c455da5d213919b4e649d365b9614
DamianCeeDevelops <rdcorbo555@gmail.com>
2024-05-07 23:10:34 -0400

    Getting arrow and going to fix it now on my desktop.

--- Canvas Project/script.js
+++ Canvas Project/script.js
@@ -9,7 +9,32 @@ ctx.fillStyle = '#F5EDED';
 
 
 function drawMenu() {
-    ctx.clearRect(0, 0, canvas, width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    for (let i = 0; i < menuOptions.length; i++) {
+        const text = menuOptions[i];
+        const textWidth = ctx.measureText(text).width;
+        const x = (canvas.width - textWidth) / 2;
 
+        if (i === selectedIndex) {
+            ctx.fillStyle = `yellow`;
+        } else {
+            ctx.fillStyle = `white`;
+        }
+
+        ctx.fillText(text, x, 30 + (i * 30));
+    }
 }
+
+document.addEventListener(`keydown`, (event) => {
+    if (event.key === 'ArrowDown') {
+        selectedIndex = Math.min(selectedIndex + 1, menuOptions.length - 1);
+    } else if (event.key == `ArrowUp`) {
+        selectedIndex = Math.max(selectedIndex - 1, 0);
+    }
+    drawMenu();
+})
+
+drawMenu();
+
+
